be3674d479f0e18c8c30c6b264943cc13791e78f
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2023-04-27 23:29:54 +0530

    Completed the project

--- mynotes/api/views.py
+++ mynotes/api/views.py
@@ -42,7 +42,7 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getNotes(request):
-    notes = Note.objects.all()
+    notes = Note.objects.all().order_by('-updated')
     serializer=NoteSerializer(notes,many=True)
     return Response(serializer.data)
 
@@ -52,3 +52,26 @@ def getNote(request,pk):
     serializer=NoteSerializer(notes,many=False)
     return Response(serializer.data)
 
+@api_view(['POST'])
+def createNote(request):
+    data = request.data
+    note=Note.objects.create(
+        body=data['body']
+        )
+    serializer = NoteSerializer(note,many=False)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def updateNote(request,pk):
+    data = request.data
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(instance=note,data=data,many=False)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def deleteNote(request,pk):
+    note = Note.objects.get(id=pk)
+    note.delete()
+    return Response("Note is deleted")
--- mynotes/mynotes/settings.py
+++ mynotes/mynotes/settings.py
@@ -2,12 +2,6 @@ from pathlib import Path
 from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -46,7 +40,9 @@ ROOT_URLCONF = 'mynotes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+        BASE_DIR/'frontend/build'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -62,8 +58,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mynotes.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -104,6 +98,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS=[
+    BASE_DIR/'frontend/build/static'
+]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
