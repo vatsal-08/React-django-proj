be9e1d32228d1a2a0a843bf3dcf8b8a0d55a8e74
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2023-06-05 22:28:41 +0530

    Recreating the full stack proj

--- mynotes/api/views.py
+++ mynotes/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from .models import Note
 from .serializers import NoteSerializer
 
+
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
@@ -40,38 +41,16 @@ def getRoutes(request):
     ]
     return Response(routes)
 
+
 @api_view(['GET'])
 def getNotes(request):
-    notes = Note.objects.all().order_by('-updated')
-    serializer=NoteSerializer(notes,many=True)
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
     return Response(serializer.data)
+
 
 @api_view(['GET'])
-def getNote(request,pk):
-    notes = Note.objects.get(id=pk)
-    serializer=NoteSerializer(notes,many=False)
-    return Response(serializer.data)
-
-@api_view(['POST'])
-def createNote(request):
-    data = request.data
-    note=Note.objects.create(
-        body=data['body']
-        )
-    serializer = NoteSerializer(note,many=False)
-    return Response(serializer.data)
-
-@api_view(['PUT'])
-def updateNote(request,pk):
-    data = request.data
+def getNote(request, pk):
     note = Note.objects.get(id=pk)
-    serializer = NoteSerializer(instance=note,data=data,many=False)
-    if serializer.is_valid():
-        serializer.save()
+    serializer = NoteSerializer(note, many=False)
     return Response(serializer.data)
-
-@api_view(['PUT'])
-def deleteNote(request,pk):
-    note = Note.objects.get(id=pk)
-    note.delete()
-    return Response("Note is deleted")
--- mynotes/mynotes/settings.py
+++ mynotes/mynotes/settings.py
@@ -1,16 +1,9 @@
+from os import environ
 from pathlib import Path
-from decouple import config
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = config('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = environ('SECRET_KEY')
 DEBUG = True
-
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -21,7 +14,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'rest_framework',
-    'corsheaders'
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -40,9 +33,7 @@ ROOT_URLCONF = 'mynotes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-        BASE_DIR/'frontend/build'
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -58,6 +49,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mynotes.wsgi.application'
 
 
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -84,11 +77,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -96,10 +84,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    BASE_DIR/'frontend/build/static'
-]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
